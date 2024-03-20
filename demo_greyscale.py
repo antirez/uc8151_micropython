9c30caefff89d47c0602aea97543589c81efebcc
antirez <antirez@gmail.com>
2024-03-20 15:46:19 +0100

    Fix filename in greyscale demo.

--- demo_greyscale.py
+++ demo_greyscale.py
@@ -6,4 +6,4 @@ spi = SPI(0, baudrate=12000000, phase=0, polarity=0, sck=Pin(18), mosi=Pin(19), 
 eink = UC8151(spi,cs=17,dc=20,rst=21,busy=26,speed=2,no_flickering=False)
 
 for greyscale in [4,8,16,32]:
-    eink.load_greyscale_image("dama.grey",greyscale)
+    eink.load_greyscale_image("dama.gs8",greyscale)
